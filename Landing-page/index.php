af752bbd941fb9ca25d3751b7242a04da0f3557e
SamGit08 <samboudiallo@ymail.com>
2017-07-28 15:01:38 +0200

    landing page

--- Landing-page/index.php
+++ Landing-page/index.php
@@ -2,10 +2,36 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
     <meta charset="utf-8">
-    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Fit'Home - Vos séances a domicile</title>
   </head>
   <body>
-
+    <div id="section1" class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <header>
+            <div class="col-xs-2">
+              <img class="logo" src="images/weights.png" alt="">
+            </div>
+            <div class="col-xs-2 col-xs-offset-5 col-sm-2 col-sm-offset-7 col-md-2 col-md-offset-8">
+              <button id="connexion" class="bouton" type="button" name="button">Connexion</button>
+            </div>
+          </header>
+        </div>
+        <div class="col-xs-12">
+          <h1>Fit'Home</h1>
+        </div>
+        <div class="col-xs-10 col-xs-offset-1">
+          <p>Amenez la salle de fitness a domicile !</p>
+        </div>
+        <div class="col-xs-6 col-xs-offset-3 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+          <button id="inscription" class="bouton" type="button" name="button">Inscription</button>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
